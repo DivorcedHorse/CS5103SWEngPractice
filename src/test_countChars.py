3056e80bd9c2e16a2762b2281aefc3ecfb9bf521
Dan <daniel.tellez99@outlook.com>
2023-03-27 10:44:52 -0500

    Minor test case updates

--- src/test_countChars.py
+++ src/test_countChars.py
@@ -28,7 +28,7 @@ class TestCharCount(unittest.TestCase):
     
     # Given an empty file, with no words or characters
     # TOTAL_CHARACTER_COUNTER should be 0 and not increment
-    def test_countEmptyFileNoLines(self):
+    def test_countEmptyFileNoCharacters(self):
         main.readFile(testCase003)
         self.assertEqual(main.TOTAL_CHARACTER_COUNTER, 0)
         self.assertGreaterEqual(1, main.TOTAL_CHARACTER_COUNTER)
@@ -74,8 +74,10 @@ class TestCharCount(unittest.TestCase):
             self.assertEqual(main.TOTAL_CHARACTER_COUNTER, number+1)
             
     # Ensures printing of total characters is correct
+    # given a file of many valid and identical  words.
     def test_printCharacterCount(self):
         main.readFile(testCase004)
+        self.assertEqual(main.TOTAL_CHARACTER_COUNTER, 41)
         expectedString = "Total number of characters counted in document: 41"
         capturedOutput = io.StringIO()
         sys.stdout = capturedOutput 
@@ -93,9 +95,10 @@ class TestCharCount(unittest.TestCase):
         self.assertGreaterEqual(1, main.TOTAL_CHARACTER_COUNTER)
         
     # Ensures printing of characters is correct even with
-    # spaces, tabs, and newlines present.
-    def test_fileCount(self):
+    # file filled with spaces, symbols, numbers, valid/invalid words.
+    def test_printCharacterCountInvalidWords(self):
         main.readFile(testCase007)
+        self.assertEqual(main.TOTAL_CHARACTER_COUNTER, 85)
         expectedString = "Total number of characters counted in document: 85"
         capturedOutput = io.StringIO()
         sys.stdout = capturedOutput 
@@ -106,9 +109,9 @@ class TestCharCount(unittest.TestCase):
         self.assertEqual(output, expectedString)
         self.assertEqual(main.TOTAL_CHARACTER_COUNTER, 85)
         self.assertNotEqual(main.TOTAL_CHARACTER_COUNTER, 0)
-
+        
     # Ensures printing of characters is correct even with
-    # file filled with spaces, symbols, numbers, valid/invalid words.
+    # spaces, tabs, and newlines present.
     def test_printCharacterCountWithSpaces(self):
         main.readFile(testCase006)
         expectedString = "Total number of characters counted in document: 52"
